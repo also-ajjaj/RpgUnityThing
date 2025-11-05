49d51e0c5e6c5ea915da6f20e5315a54b0e480a1
ajjaj <129668496+also-ajjaj@users.noreply.github.com>
2025-11-05 11:23:58 +0100

    Animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,12 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _rigidBody;
     private Vector2 _direction;
+    private SpriteAnimator _spriteAnimator;
     public float speed;
 
     private void Awake()
     {
+        _spriteAnimator = GetComponent<SpriteAnimator>();
         _rigidBody = GetComponent<Rigidbody2D>();
     }
 
@@ -16,20 +18,30 @@ public class PlayerMovement : MonoBehaviour
     {
         _direction = new Vector2(0, 0);
         
+        
         if (Input.GetKey(KeyCode.W))
         {
             _direction.y = 1;
+            _spriteAnimator.PlayAnimation("WalkUp");
         }else if (Input.GetKey(KeyCode.S))
         {
             _direction.y = -1;
+            _spriteAnimator.PlayAnimation("WalkDown");
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             _direction.x = -1;
+            _spriteAnimator.PlayAnimation("WalkLeft");
         }else if (Input.GetKey(KeyCode.D))
         {
             _direction.x = 1;
+            _spriteAnimator.PlayAnimation("WalkRight");
+        }
+
+        if (_direction == Vector2.zero)
+        {
+            _spriteAnimator.PlayAnimation("Idle");
         }
     }
 
